20f77a6b9d39ac986e06f967050fbc2f4d902df3
Jenniffer Martins <eujennifferlino@gmail.com>
2026-07-15 01:32:35 -0300

    refactor: organize Bryqo project structure

---
+++ bryco-app/App/bryco_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  bryco_appApp.swift
+//  bryco-app
+//
+//  Created by Jenniffer Martins on 15/07/26.
+//
+
+import SwiftUI
+
+@main
+struct bryco_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
